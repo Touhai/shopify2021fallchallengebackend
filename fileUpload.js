430d2057016df5421f745ef28a4cb5e690d0f27d
Touhai <bryanwkou@gmail.com>
2021-05-06 12:23:59 -0700

    Added fileUpload to S3

--- fileUpload.js
+++ fileUpload.js
@@ -2,18 +2,39 @@
 
 const fs = require('fs');
 const AWS = require('aws-sdk');
+const path = require('path');
 require('dotenv').config();
 
 const BUCKET_NAME = "bryan-shopify-challenge-2021";
-const IAM_USER_KEY = process.env.AWS_USERKEY;
-const IAM_USER_SECRET = process.env.AWS_SECRET;
 
 AWS.config.update({region: 'us-east-1'});
 
+//Creates an S3 bucket
 const s3 = new AWS.S3();
 
+// const file = "InsertFileNameHere"
+
+function upload (fileName){
+    let uploadParams = {Bucket: BUCKET_NAME, Key:'',Body:''};
+    let fileStream = fs.createReadStream(fileName);
+    fileStream.on('error', function(err){
+        console.log("File error", err);
+    });
+    uploadParams.Body = fileStream;
+    uploadParams.Key = path.basename(fileName);
 
 
+    s3.upload(uploadParams, function(err,data){
+        if (err){
+            console.log("error", err);
+        } else if (data){
+            console.log("Uploaded to", data.Location)
+        }
+    })
+}
+
+
+// Debug statements to see if I can auth to S3.
 s3.listBuckets(function(err,data){
     if(err){
         console.log("error",err);
@@ -22,6 +43,6 @@ s3.listBuckets(function(err,data){
     }
 });
 
-
-
+//Uploads the file
+//  upload(file);
 
